37456e093345a3607ccbbe8d32a2b92555f3808b
adrtkach <adr.tkachh@gmail.com>
2022-08-26 16:17:36 +0300

    Initial commit

---
+++ pages/homePage.ts
@@ -0,0 +1,47 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class HomePage {
+
+    readonly page: Page;
+    readonly navCustomShop: Locator;
+    readonly navBlog: Locator;
+    readonly navAboutUs: Locator;
+    readonly navContacts: Locator;
+    readonly menuBracelets: Locator;
+    readonly menuEarrings: Locator;
+    readonly menuNecklaces: Locator;
+    readonly menuRings: Locator;
+    readonly searchButton: Locator;
+    readonly cartButton: Locator;
+    readonly searchInput: Locator;
+    readonly searchCloseButton: Locator;
+    readonly searchSubmitButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+
+        // Search Field
+        this.searchButton = page.locator('.header-search__submit-button');
+        this.searchInput = page.locator('#header-search__input');
+        this.searchCloseButton = page.locator('#header-search__close-button');
+        this.searchSubmitButton = page.locator('#header-search__submit-button');
+
+
+        // Cart Button
+        this.cartButton = page.locator('.header__cart .header__cart-button');
+        
+
+        // Navigation Links
+        this.navCustomShop = page.locator('.header__navigation a:has-text("Custom Shop")');
+        this.navBlog = page.locator('.header__navigation a:has-text("Blog")');
+        this.navAboutUs = page.locator('.header__navigation a:has-text("About Us")');
+        this.navContacts = page.locator('.header__navigation a:has-text("Contacts")');
+
+        // Navigation Menu
+        this.menuBracelets = page.locator('.header__menu a:has-text("Bracelets")');
+        this.menuEarrings = page.locator('.header__menu a:has-text("Earrings")');
+        this.menuNecklaces = page.locator('.header__menu a:has-text("Necklaces & Pendants")');
+        this.menuRings = page.locator('//*[@id="header"]/div[2]/div[2]/div[4]/a[text()="Rings"]');
+    }
+
+}
---
+++ tests/homepage/homePage.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect, Page } from '@playwright/test';
+import { HomePage } from '../../pages/homePage';
+
+const mainUrl: String = 'http://develop.diamantllc.us';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(`${mainUrl}`);
+    retries: 3;
+});
+
+test('Search button opens Search field', async ({ page }) => {
+
+    const homePage = new HomePage(page);
+
+    await expect (homePage.searchButton, 'incorrect searchButton link').toHaveAttribute('data-href', 'https://develop.diamantllc.us/search');
+
+    const NotActivatedSearchAssert = async function() {
+        expect(homePage.searchInput).toHaveCSS('display', 'none'); // no AWAIT because it calls the function
+        expect(homePage.searchCloseButton).toHaveCSS('display', 'none');
+    }
+
+    const ActivatedSearchAssert = async function() {
+        expect(homePage.searchInput).not.toHaveCSS('display', 'none');
+        expect(homePage.searchCloseButton).not.toHaveCSS('display', 'none'); 
+    } 
+
+    await NotActivatedSearchAssert();
+    await homePage.searchButton.click(); // activate search
+    await ActivatedSearchAssert();
+    await homePage.searchCloseButton.click(); // deactivate search
+    await NotActivatedSearchAssert();
+});
+
+test.skip('Cart link', async ({ page }) => {
+
+    const homePage = new HomePage(page);
+    await expect(homePage.cartButton, 'incorrect cartButton link').toHaveAttribute('href', `${mainUrl}/cart`);
+});
+
+test.skip('Navigation links', async ({ page }) => {
+
+    const homePage = new HomePage(page);
+    await expect.soft(homePage.navCustomShop, 'incorrect navCustomShop link').toHaveAttribute('href', `${mainUrl}/custom-shop`);
+    await expect.soft(homePage.navBlog, 'incorrect navBlog link').toHaveAttribute('href', `${mainUrl}/blog`);
+    await expect.soft(homePage.navAboutUs, 'incorrect navAboutUs link').toHaveAttribute('href', `${mainUrl}/about-us`);
+    await expect.soft(homePage.navContacts, 'incorrect navContacts link').toHaveAttribute('href', `${mainUrl}/contacts`);
+});
+
+test.skip('Menu links', async ({ page }) => {
+
+    const homePage = new HomePage(page);
+    await expect.soft(homePage.menuBracelets, 'incorrect menuBracelets link').toHaveAttribute('href', `${mainUrl}/jewelry/bracelets`);
+    await expect.soft(homePage.menuEarrings, 'incorrect menuEarrings link').toHaveAttribute('href', `${mainUrl}/jewelry/earrings`);
+    await expect.soft(homePage.menuNecklaces, 'incorrect menuNecklaces link').toHaveAttribute('href', `${mainUrl}/jewelry/necklaces-pendants`);
+    await expect.soft(homePage.menuRings, 'incorrect menuRings link').toHaveAttribute('href', `${mainUrl}/jewelry/rings`);
+});
